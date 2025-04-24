970129f2ef07474dbebfb13532cb54c7ec3c4640
=Prachanda Rana <=>
2025-04-24 12:54:38 +0545

    ip setting post

--- src/routes/reportRouter.route.js
+++ src/routes/reportRouter.route.js
@@ -9,6 +9,6 @@ import { upload } from "../middleware/multer.middleware.js";
 const reportRouter = Router();
 reportRouter.route("/").get(reportDetails);
 reportRouter.route("/create-report").post(upload.single("image"), createReport);
-reportRouter.route("/ip-setting").get(getIpInfo);
+reportRouter.route("/ip-setting").post(getIpInfo);
 
 export default reportRouter;
