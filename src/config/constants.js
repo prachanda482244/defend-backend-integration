a26cc2f515d71375b2c96064e8fe0718bd8fc048
=Prachanda Rana <=>
2025-04-28 16:38:46 +0545

    change the db url

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,4 +3,4 @@ import { config } from "dotenv";
 config();
 export const PORT = process.env.PORT || 5000;
 export const DB_NAME = "";
-export const MONGODB_URI = process.env.MONGODB_LOCAL_URI || "";
+export const MONGODB_URI = process.env.MONGODB_URI || "";
